7dbb11d3072934cc4868f73bb56398c8d3b21498
jagathjyotish <86792395+jagathjyotish@users.noreply.github.com>
2022-11-01 09:46:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+varball;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	//engine = Engine.create();
+	//world = engine.world;
+
+	//Create the Bodies Here.
+    var ball_options = {
+		isStatic:false,
+		restituton:0.3,
+		friction:0,
+	}
+
+
+	//Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
